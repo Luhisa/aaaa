77998be01433d59e679d4c245e2c17bf5f906609
Luisa Santos <luisavtrsantos@gmail.com>
2023-06-28 09:56:11 -0300

    atualizade

---
+++ js/script.js
@@ -0,0 +1,139 @@
+//MINIMO DE PRODUTOS (1):
+let modalQt = 1;
+//
+let cart = [];
+let modalKey = 0;
+
+const c = (el) => document.querySelector(el);
+const cs = (el) => document.querySelectorAll(el);
+
+// Listagem dos modelos
+
+modeloJson.map((item, index) => {
+    let modeloItem = c('.models .modelo-item').cloneNode(true);
+
+    modeloItem.setAttribute('data-key', index);
+
+    modeloItem.querySelector('.modelo-item-img img').src = item.img;
+    modeloItem.querySelector('.modelo-item-name').innerHTML = item.name;
+    modeloItem.querySelector('.modelo-item-desc').innerHTML = item.description;
+    modeloItem.querySelector('.modelo-item-price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+
+    modeloItem.querySelector('a').addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let key = e.target.closest('.modelo-item').getAttribute('data-key');
+        modalQt = 1; //quantidade de produto ao abrir o modal
+        modalKey = key;
+
+        c('.modeloBig img').src = modeloJson[key].img;
+        c('.modeloInfo h1').innerHTML = modeloJson[key].name;
+        c('.modeloInfo-desc').innerHTML = modeloJson[key].description;
+        c('.modeloInfo-actualPrice').innerHTML = `R$ ${modeloJson[key].price.toFixed(2)}`;
+        c('.modeloInfo-size.selected').classList.remove('selected');
+
+        cs('.modeloInfo-size').forEach((size, sizeIndex) => {
+            if (sizeIndex == 0){  /*COM ISSO, POR PADRÃO O "P" FICARA SELECIONADO (AZUL) AO CLICAR NO PRODUTO*/
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = modeloJson[key].sizes[sizeIndex];
+        });
+
+        //quantidade minima de produto:
+        c('.modeloInfo-qt').innerHTML = modalQt;
+        //opacidade da aba do carrinho:
+        c('.modeloWindowArea').style.opacity = 0;
+        c('.modeloWindowArea').style.display = 'flex';
+
+        setTimeout(() => {
+            c('.modeloWindowArea').style.opacity = 1;
+        }, 200);
+        });
+
+        c('.modelo-area').append(modeloItem);
+});
+
+//eventos do Modal
+function closeModal(){
+    c('.modeloWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        c('.modeloWindowArea').style.display = 'none';
+    }, 500);
+}
+
+cs('.modeloInfo-cancelButton, .modeloInfo-cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal());
+});
+
+//botão qt menos
+c('.modeloInfo-qtmenos').addEventListener('click', () =>{
+    if (modalQt > 1){
+        modalQt--;
+        c('.modeloInfo-qt').innerHTML = modalQt;
+    }
+});
+
+//botão qt mais
+c('.modeloInfo-qtmais').addEventListener('click', () =>{
+    modalQt++;
+    //atualizo o valor no html
+    c('.modeloInfo-qt').innerHTML = modalQt;
+});
+
+//tamanhos
+cs('.modeloInfo-size').forEach((size, sizeIndex) => {
+    //ao clicar em um item
+    size.addEventListener('click', () => {
+        //desmarca tudo
+        c('.modeloInfo-size.selected').classList.remove('selected');
+        //adicionar a marcação
+        size.classList.add('selected');
+    });
+});
+
+//adicionar ao carrinho
+c('.modeloInfo-addButton').addEventListener('click', () => {
+    //reunir as informações: modelo, tamanho, qtd
+    let size = c('.modeloInfo-size.selected').getAttribute('data-key');
+
+    //juntar id e tamanho
+    let identifier = modeloJson[modalKey].id + '/' + size;
+
+    //verificar se já está no carrinho
+    let key = cart.findIndex((item) => {
+        return item.identifier == identifier;
+    })
+
+    //verificar se é igual ou não
+    if(key > -1){
+        cart[key].qt += modalQt;
+    }else{
+    //adicionar ao carrinho
+        cart.push({
+            identifier,
+            id: modeloJson[modalKey].id, 
+            size, 
+            qt: modalQt,
+        });
+    }
+    updateCart();
+    closeModal();
+});
+
+//Atualizar carrinho
+function updateCart(){
+    if(cart.length > 0){
+        c('aside').classList.add('show');
+    }else{
+        c('aside').classList.remove('show');
+    }
+}
+
+
+    
+
+
+
+
+
+
